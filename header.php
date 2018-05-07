4d4618cd8ffaf7fcd9065f35c93a566d53c05ad9
gam3ov3r <reklam@proclaim.se>
2018-05-07 16:25:59 +0200

    Uppdaterade menyn

--- header.php
+++ header.php
@@ -18,12 +18,28 @@
 	<header>
 		<a href="<?php echo get_home_url(); ?>"><img src="<?php echo img; ?>/logo.svg" class="logo"></a>
 		<nav>
-			<ul class="list-inline">
-				<li><a href="#">Om oss</a></li>
-				<li><a href="#">Varför</a></li>
-				<li><a href="#">Kunder</a></li>
-				<li><a href="#">Nyheter</a></li>
-				<li><a href="#">Kontakt</a></li>				
-			</ul>
+			<ul class="list-inline<?php if ( is_front_page() ) { echo " home"; }?>">
+			<?php
+				$defaults = array(
+					'theme_location' => 'main-menu',
+					'menu'            => '',
+					'container'       => '',
+					'container_class' => '',
+					'container_id'    => '',
+					'menu_class'      => 'list-inline',
+					'menu_id'         => '',
+					'echo'            => true,
+					'fallback_cb'     => 'wp_page_menu',
+					'before'          => '',
+					'after'           => '',
+					'link_before'     => '',
+					'link_after'      => '',
+					'items_wrap'      => '%3$s',
+					'depth'           => 0,
+					'walker'          => ''
+				);
+				wp_nav_menu( $defaults );
+			?>
+			</ul>			
 		</nav>
 	</header>
