ae159840d3722801b3baf14bd646439e6503d66c
gam3ov3r <reklam@proclaim.se>
2018-05-14 16:05:50 +0200

    Lagt in stöd för ”Safari Beauty Toolbar”: https://github.com/neSpecc/safari-beauty-toolbar/blob/master/build/sct.min.js

--- _inc/js.php
+++ _inc/js.php
@@ -12,12 +12,16 @@ function load_scripts() {
 		wp_register_script('bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', array('jquery'), null, 'all' );
 		
 		wp_register_script('js', get_template_directory_uri() . '/js/script.min.js', array('bootstrap'), null, 'all' );
+		wp_register_script('sct', get_template_directory_uri() . '/js/sct.min.js', array('bootstrap'), null, 'all' );
 			
 		// Load our Javascript
 		wp_enqueue_script('jquery');
 		wp_enqueue_script('bootstrap');
-				
+		
+		wp_enqueue_script('sct');		
 		wp_enqueue_script('js');
+		
+		
 	}
 }
 add_action('init', 'load_scripts');
