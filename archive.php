48fcad61f2a0a2c3e8465c7b8718e1c0b6201c06
gam3ov3r <reklam@proclaim.se>
2018-05-21 14:34:54 +0200

    No message

---
+++ archive.php
@@ -0,0 +1,31 @@
+<?php
+/* 
+Template Name: Archives
+*/
+get_header(); ?>
+<div class="container">
+	<div class="col-md-8 col-md-offset-2">
+<?php 
+    query_posts(array(
+		'post_type' => array('post'), 
+		'posts_per_page' => -1
+    ));  
+	if ( have_posts() ) : while ( have_posts() ) : the_post();
+?>
+
+	<article class="post" id="post-<?php the_ID(); ?>">
+	<a href="<?php the_permalink();?>">		
+	<h2><?php the_title(); ?></h2>
+	<?php the_excerpt();?>
+	</a>
+	</article>
+
+<?php endwhile; endif; wp_reset_query(); ?>
+	</div>
+</div>
+<!--
+    ###################
+	###  /LOOP END  ### 
+	###################
+-->
+<?php get_footer(); ?>
