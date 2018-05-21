c1aa5cc7ad6f705d43d8202f0b77a6ae4a52253e
gam3ov3r <reklam@proclaim.se>
2018-05-21 14:35:16 +0200

    No message

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php get_header(); ?>
+<?php query_posts($query_string); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<div class="container">
+	<div class="col-md-8 col-md-offset-2">
+	<article class="post" id="post-<?php the_ID(); ?>">
+		<?php
+			if ( has_post_thumbnail() ) {
+				$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'your_thumb_handle' );
+				echo '<img src="' . $thumbnail['0'] . '" class="img-responsive">';
+			}
+		?>			
+		<h1><?php the_title(); ?></h1>
+		<?php the_content('Read more →'); ?>
+	</article>
+	</div>
+</div>
+<?php endwhile; endif; wp_reset_query(); ?>
 <?php get_footer(); ?>
